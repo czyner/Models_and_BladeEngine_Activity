bdfac29058ebf503c40be7242130879840e58476
czyner <czyner7@gmail.com>
2024-04-14 20:14:15 +0800

    Routes, Controllers created, defined

--- Models_and_BladeEngine_Activity/Models_and_BladeEngine_Activity/app/Http/Controllers/BookController.php
+++ Models_and_BladeEngine_Activity/Models_and_BladeEngine_Activity/app/Http/Controllers/BookController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Book;
 
 class BookController extends Controller
 {
-    //
+    public function index(){
+        $data = DB::table('books')->select('id', 'isbn',
+        'title', 'author', 'description',
+        'date_published')->get();
+        DD($data);
+        return view('books.index', ['books' => $data]);
+    }
 }
--- Models_and_BladeEngine_Activity/Models_and_BladeEngine_Activity/routes/web.php
+++ Models_and_BladeEngine_Activity/Models_and_BladeEngine_Activity/routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\http\Controllers\BookController;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', function (){
+    return view('book');
+});
+
+Route::get('/book', [BookController::class, 'index']);
